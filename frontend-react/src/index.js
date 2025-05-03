90f2e914e33c419ccdc3d33ad58fcf332025713b
nasssima2002 <nassimataarabet2003@gmail.com>
2025-05-03 01:50:46 +0100

    tetser

--- frontend-react/src/index.js
+++ frontend-react/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';  // Les styles globaux
+import './App.css';  // Les styles globaux
 import App from './App';  // Le composant principal
 import { BrowserRouter } from 'react-router-dom';  // Pour React Router
 
